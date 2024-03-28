0483fcda34c454896fa187380a42b9c255a459c4
AndiBagasPrayogo <161208174+AndiBagasPrayogo@users.noreply.github.com>
2024-03-28 12:38:56 +0800

    Add files via upload

---
+++ Tugas 2/Source Code/Tugas 2 BufferedReader.java
@@ -0,0 +1,35 @@
+/* Nim : 13020220077
+Nama : Andi Bagas Prayogo
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReader1{
+	public static void main(String[] args){
+	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	String Nim="";
+	String Nama="";
+	String Jurusan="";
+	String Fakultas="";
+
+	try{
+	System.out.print("Masukkan Nama : ");
+	Nama=input.readLine();
+	System.out.print("Masukkan Nim : ");
+	Nim=input.readLine();
+	System.out.print("Masukkan Jurusan : ");
+	Jurusan=input.readLine();
+	System.out.print("Masukkan Fakultas : ");
+	Fakultas=input.readLine();
+	}catch (Exception e){}
+
+	System.out.println("=====================");
+	System.out.println("Nama    : "+ Nama);
+	System.out.println("Nim     : "+ Nim);
+	System.out.println("Jurusan : "+ Jurusan);
+	System.out.println("Fakultas: "+ Fakultas);
+
+  }
+}
